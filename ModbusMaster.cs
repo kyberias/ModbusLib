4b82c83f482a9e046b3d82642c33bc581e83032c
Jaakko Haakana <jaakko@haakana.net>
2026-09-15 07:38:44 +0300

    Initial commit

---
+++ ModbusMaster.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Buffers.Binary;
+using System.Net;
+using System.Net.Sockets;
+
+namespace meterEmul;
+
+public class ModbusMaster : IDisposable
+{
+    private readonly int port;
+    private readonly ILogger log;
+
+    private readonly IPAddress ipAddress;
+
+    public ModbusMaster(int port, IPAddress ipAddress, ILogger log)
+    {
+        this.port = port;
+        this.ipAddress = ipAddress;
+        this.log = log;
+    }
+
+    private TcpClient client;
+
+    public async Task Start(CancellationToken cancel)
+    {
+        client = new TcpClient();
+
+        while (true)
+        {
+            try
+            {
+                await client.ConnectAsync(new IPEndPoint(ipAddress, port), cancel);
+                return;
+            }
+            catch (SocketException ex)
+            {
+                log.LogWarning(ex, "Connect failed. Retrying");
+                await Task.Delay(TimeSpan.FromSeconds(5), cancel);
+            }
+        }
+    }
+
+    const byte ModbusFcReadMultipleRegisters = 0x03;
+    private const byte ModbusFcWriteMultipleRegisters = 16;
+
+    private int transactionId = 0;
+
+    public async Task<IEnumerable<ushort>> ReadMultipleRegisters(ushort addr, ushort num)
+    {
+        var aduFrame = new byte[12];
+
+        BinaryPrimitives.WriteUInt16BigEndian(aduFrame, (ushort)transactionId);
+        transactionId++;
+
+        // protocol identifier
+        BinaryPrimitives.WriteUInt16BigEndian(aduFrame.AsSpan(2), 0);
+
+        // message length, 6 bytes to follow
+        BinaryPrimitives.WriteUInt16BigEndian(aduFrame.AsSpan(4), 6);
+
+        byte unit = 1;//0xC8;
+        aduFrame[6] = unit;
+
+        aduFrame[7] = ModbusFcReadMultipleRegisters;
+
+        BinaryPrimitives.WriteUInt16BigEndian(aduFrame.AsSpan(8), addr);
+        BinaryPrimitives.WriteUInt16BigEndian(aduFrame.AsSpan(10), num);
+
+        var stream = client.GetStream();
+
+        stream.Write(aduFrame);
+
+        log.LogTrace($"ReadMultipleRegisters {addr} {num}");
+        log.LogTrace(string.Join(" ", aduFrame.Select(b => b.ToString("x2"))));
+
+        await stream.ReadExactlyAsync(aduFrame, 0, 7);
+
+        var messageLen =
+            BinaryPrimitives.ReadUInt16BigEndian(aduFrame.AsSpan(4));
+
+        var pdu = new byte[messageLen-1];
+
+        await stream.ReadExactlyAsync(pdu);
+
+        var responseFunctionCode = pdu[0];
+        if ((0x80 & responseFunctionCode) > 0)
+        {
+            Console.WriteLine($"Read error {responseFunctionCode:X} {pdu[1]:X}");
+            Console.WriteLine($"Readind from {addr} {num}");
+        }
+
+        var receivedLen = pdu[1];
+
+        var result = new ushort[receivedLen / 2];
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = BinaryPrimitives.ReadUInt16BigEndian(pdu.AsSpan(2 + i*2));
+        }
+
+        return result;
+    }
+
+    public async Task<int> WriteMultipleRegisters(ushort addr, params ushort[] regs)
+    {
+        log.LogDebug($"Write {addr} {regs[0]}");
+
+        var fullFrame = new byte[6*2 + regs.Length * 2 + 1];
+
+        BinaryPrimitives.WriteUInt16BigEndian(fullFrame.AsSpan(), (ushort)transactionId++);
+        BinaryPrimitives.WriteUInt16BigEndian(fullFrame.AsSpan(2), 0);
+        BinaryPrimitives.WriteUInt16BigEndian(fullFrame.AsSpan(4), (ushort)(6 + 2 * regs.Length + 1));
+
+        // unit identifier
+        fullFrame[6] = 1;
+        // function code
+        fullFrame[7] = ModbusFcWriteMultipleRegisters;
+
+        // address
+        BinaryPrimitives.WriteUInt16BigEndian(fullFrame.AsSpan(8), addr);
+        // number of regs
+        BinaryPrimitives.WriteUInt16BigEndian(fullFrame.AsSpan(10), (ushort)regs.Length);
+
+        fullFrame[12] = (byte)(2 * regs.Length);
+
+        for (int i = 0; i < regs.Length; i++)
+        {
+            BinaryPrimitives.WriteUInt16BigEndian(fullFrame.AsSpan(13 + i), regs[i]);
+        }
+
+        var stream = client.GetStream();
+        stream.Write(fullFrame);
+
+        await stream.ReadExactlyAsync(fullFrame, 0, 9);
+
+        var responseFunctionCode = fullFrame[7];
+
+        if ((0x80 & responseFunctionCode) > 0)
+        {
+            Console.WriteLine($"Error {fullFrame[8]}");
+            return 0;
+        }
+
+        await stream.ReadExactlyAsync(fullFrame, 9, 3);
+
+        var written = BinaryPrimitives.ReadUInt16BigEndian(fullFrame.AsSpan(10));
+
+        log.LogDebug($"Written {written}");
+        return written;
+    }
+
+    public void Dispose()
+    {
+        client.Dispose();
+    }
+}
---
+++ ModbusSlave.cs
@@ -0,0 +1,197 @@
+﻿using System.Buffers.Binary;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace meterEmul;
+
+public class ModbusSlave
+{
+    private readonly int port;
+    private readonly ILogger log;
+
+    private readonly IPAddress ipAddress;
+
+    public ModbusSlave(int port, string? ipAddress, ILogger log)
+    {
+        this.log = log;
+        this.port = port;
+
+        this.ipAddress = !string.IsNullOrEmpty(ipAddress) ? IPAddress.Parse(ipAddress) : IPAddress.Any;
+    }
+
+    public async Task Run(CancellationToken cancel)
+    {
+        var ipEndPoint = new IPEndPoint(ipAddress, port);
+        TcpListener listener = new(ipEndPoint);
+
+        List<Task> clients = new List<Task>();
+
+        listener.Start();
+        var acceptTask = listener.AcceptTcpClientAsync(cancel);
+
+        while (!cancel.IsCancellationRequested)
+        {
+            var completedTask = await Task.WhenAny(clients.Concat(new[] { acceptTask.AsTask() }));
+
+            if (acceptTask.IsCompleted)
+            {
+                var newClient = await acceptTask;
+
+                clients.Add(ClientTask(newClient, cancel));
+
+                acceptTask = listener.AcceptTcpClientAsync(cancel);
+            }
+            else
+            {
+                try
+                {
+                    await completedTask;
+                }
+                catch (Exception ex)
+                {
+                    log.LogWarning(ex, ex.Message);
+                }
+
+                clients.Remove(completedTask);
+            }
+        }
+
+        listener.Stop();
+    }
+
+    private const byte ModbusFcReadMultipleRegisters = 0x03;
+    private const int ModbusMaxHoldingRegisters = 65536;
+
+    private readonly ConcurrentDictionary<ushort, ushort> holdingRegisterValues = new ();
+
+    public async Task<ushort> SetHoldingRegister(ushort addr, ushort value)
+    {
+        holdingRegisterValues[addr] = value;
+        return (ushort)(addr + 1);
+    }
+
+    public Task<ushort> SetHoldingRegister(ushort addr, float value)
+    {
+        log.LogInformation($"SetHoldingRegister {addr} = {value}");
+
+        var bits = BitConverter.SingleToUInt32Bits(value);
+
+        holdingRegisterValues[addr] = (ushort)(bits >> 16);
+        holdingRegisterValues[(ushort)(addr+1)] = (ushort)(bits & 0xFFFF);
+        return Task.FromResult((ushort)(addr + 2));
+    }
+
+    public async Task<ushort> SetHoldingRegister(ushort addr, string value, int words)
+    {
+        var maxBytes = words * 2;
+        var stringBytes = Encoding.UTF8.GetBytes(value);
+
+        if (stringBytes.Length > maxBytes)
+        {
+            stringBytes = stringBytes.Take(maxBytes).ToArray();
+        }
+
+        var bytes = stringBytes.Concat(Enumerable.Range(0, maxBytes - stringBytes.Length).Select(a => (byte)0)).ToArray();
+
+        for (int i = 0; i < words; i++)
+        {
+            holdingRegisterValues[(ushort)(addr + i)] = BinaryPrimitives.ReadUInt16BigEndian(bytes.AsSpan(i * 2));
+        }
+
+        return (ushort)(addr + words);
+    }
+
+    async Task ClientTask(TcpClient client, CancellationToken cancel)
+    {
+        log.LogInformation($"New client from {client.Client.RemoteEndPoint}");
+        var stream = client.GetStream();
+
+        var aduFrame = new byte[7];
+
+        while (!cancel.IsCancellationRequested)
+        {
+            // Read ADU = Application Data Unit = Additional address + PDU + error check
+
+            await stream.ReadExactlyAsync(aduFrame, cancel);
+
+            // 0-1 Transaction ID 
+            // 2-3 Protocol ID
+            // 4-5 Length
+            // 6 Unit ID
+            // 7 Function Code
+            // 8 Data length
+            // 9-.. Data...
+
+            var transactionId = BinaryPrimitives.ReadUInt16BigEndian(aduFrame);
+            var protocolId = BinaryPrimitives.ReadUInt16BigEndian(aduFrame.AsSpan(2));
+            var length = BinaryPrimitives.ReadUInt16BigEndian(aduFrame.AsSpan(4));
+            var unitId = aduFrame[6];
+
+            log.LogDebug($"ADU frame: {transactionId} {protocolId} {length} {unitId}");
+
+            var data = new byte[length-1];
+
+            await stream.ReadExactlyAsync(data, cancel);
+
+            var function = data[0];
+            log.LogDebug($"Function: {function:X}");
+
+            switch (function)
+            {
+                case ModbusFcReadMultipleRegisters:
+                    var addr = BinaryPrimitives.ReadUInt16BigEndian(data.AsSpan(1));
+                    var num = BinaryPrimitives.ReadUInt16BigEndian(data.AsSpan(3));
+
+                    log.LogDebug($"ReadMultipleRegisters addr:{addr} num:{num}");
+
+                    BinaryPrimitives.WriteUInt16BigEndian(aduFrame, transactionId);
+                    BinaryPrimitives.WriteUInt16BigEndian(aduFrame.AsSpan(2), protocolId);
+                    BinaryPrimitives.WriteUInt16BigEndian(aduFrame.AsSpan(4), (ushort)(num * 2 + 3));
+                    aduFrame[6] = unitId;
+
+                    await stream.WriteAsync(aduFrame, cancel);
+
+                    bool errorSent = false;
+
+                    /*for (int i = 0; i < num; i++)
+                    {
+                        if (!holdingRegisterValues.ContainsKey((ushort)(addr + i)))
+                        {
+                            var errorData = new byte[2];
+                            errorData[0] = (byte)(function | 0x80);
+                            errorData[1] = 0x02; // Exception: illegal data address
+                            await stream.WriteAsync(errorData, cancel);
+                            errorSent = true;
+                            break;
+                        }
+                    }*/
+
+                    if (errorSent)
+                    {
+                        break;
+                    }
+
+                    var responseData = new byte[2 + num*2];
+
+                    responseData[0] = function;
+                    responseData[1] = (byte)(num * 2);
+                    for (int i = 0; i < num; i++)
+                    {
+                        var regaddr = (ushort)(addr + i);
+                        BinaryPrimitives.WriteUInt16BigEndian(responseData.AsSpan(2 + i*2), holdingRegisterValues.ContainsKey(regaddr) ? holdingRegisterValues[regaddr] : (ushort)0);
+                    }
+
+                    await stream.WriteAsync(responseData, cancel);
+
+                    break;
+                default:
+                    log.LogWarning($"Unknown Modbus function: {function}");
+                    break;
+            }
+        }
+    }
+
+}
